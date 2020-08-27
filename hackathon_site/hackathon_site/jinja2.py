1698d2ec1b230dd9556ade1c313882129ec13a76
Lisa Li <lisasiaili@gmail.com>
2020-08-26 21:15:38 -0400

    added imgur images for carousel

--- hackathon_site/hackathon_site/jinja2.py
+++ hackathon_site/hackathon_site/jinja2.py
@@ -1,7 +1,7 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.conf import settings
 from django.urls import reverse
-from django.utils.timezone import template_localtime
+from django.utils.timezone import template_localtime, now
 
 from jinja2 import Environment
 
@@ -19,6 +19,7 @@ def environment(**options):
             "registration_close_date": settings.REGISTRATION_CLOSE_DATE,
             "event_start_date": settings.EVENT_START_DATE,
             "event_end_date": settings.EVENT_END_DATE,
+            "now": now(),
         }
     )
     return env
