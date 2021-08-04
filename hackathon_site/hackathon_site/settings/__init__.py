9c4a53e8965afa1ca2dcf6b075d8f772e6cefedf
Lisa Li <lisasiaili@gmail.com>
2021-08-03 22:32:35 -0400

    remove default from email

--- hackathon_site/hackathon_site/settings/__init__.py
+++ hackathon_site/hackathon_site/settings/__init__.py
@@ -259,7 +259,6 @@ LOGGING = {
 
 # Event specific settings
 HACKATHON_NAME = "NewHacks"
-DEFAULT_FROM_EMAIL = "webmaster@localhost"
 CONTACT_EMAIL = DEFAULT_FROM_EMAIL
 
 REGISTRATION_OPEN_DATE = datetime(2021, 9, 17, tzinfo=pytz.timezone(TIME_ZONE))
